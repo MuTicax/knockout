f9fd710f8706b1553bfaf1a2527397925c0a8334
MuTicax <muticax@gmail.com>
2023-06-11 22:14:33 +0200

    Main

---
+++ fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+
+author 'MuTicax'
+description 'Knockout Script'
+
+shared_scripts {'@ox_lib/init.lua'}
+
+client_scripts {'cl_knockout.lua', 'config.lua'}
